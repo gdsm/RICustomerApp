5ef0827f01d235656885e9872f863d8b29fff652
gdsm <gagandeepsinghmadan@gmail.com>
2017-10-23 19:30:50 -0500

    Added field for report a problem

---
+++ Shared/Classes/UI/ViewControllers/EquipmentService/ReportProblemViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReportProblemViewController.h
+//  ReddyIce
+//
+//  Created by Gagan on 23/10/17.
+//  Copyright © 2017 HCL. All rights reserved.
+//
+
+#import "BaseTableViewController.h"
+
+@interface ReportProblemViewController : BaseTableViewController
+
+@end
---
+++ Shared/Classes/UI/ViewControllers/EquipmentService/ReportProblemViewController.m
@@ -0,0 +1,37 @@
+//
+//  ReportProblemViewController.m
+//  ReddyIce
+//
+//  Created by Gagan on 23/10/17.
+//  Copyright © 2017 HCL. All rights reserved.
+//
+
+#import "ReportProblemViewController.h"
+
+@interface ReportProblemViewController ()
+
+@end
+
+@implementation ReportProblemViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
